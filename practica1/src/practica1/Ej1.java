1855ef7715d827bf8a0f1623bd017e1c2b662e0a
Ivan <ivansongohan77@gmail.com>
2021-03-12 14:49:52 +0100

    Cambio1

--- practica1/src/practica1/Ej1.java
+++ practica1/src/practica1/Ej1.java
@@ -2,8 +2,10 @@ package practica1;
 
 public class Ej1 {
 	public static void main(String[] args) {
-		
-		System.out.println("Hola mundo java-git");
-	}
+		 System.out.println("Hola mundo java-git");
+		 System.out.println("Hola mundo2 java-git");
+		 System.out.println("Hola mundo3 java-git");
+		 System.out.println("Pruebas Git. Se añade Cambio 1");
+		 } 
 
 }
